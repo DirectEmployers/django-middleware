316cb866042be1dfa4b88383455bc70bdee71b66
Tim Loyer <tim.loyer@gmail.com>
2022-04-19 11:36:04 -0400

    make it a package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="django-utils",
+    version="1.0.0",
+    description="Reusable Django utilities for DE apps.",
+    url="https://github.com/DirectEmployers/django-utils",
+    license="Copyright © 2011-2022, DirectEmployers Association",
+    packages=find_packages(),
+)
