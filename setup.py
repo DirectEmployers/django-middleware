3a20f61508b8bfd54d19e07ba328cb3f77caf527
Tim Loyer <tloyer@directemployers.org>
2024-01-03 14:34:24 -0500

    Update requirements to support django 4.2 (#6)
    * update minimum requirements
    
    * pin 4.2.x

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import find_packages, setup
 
 setup(
     name="django-middleware",
-    version="1.0.0",
+    version="4.2",
     description="Reusable Django utilities for DE apps.",
-    url="https://github.com/DirectEmployers/django-utils",
-    license="Copyright © 2011-2022, DirectEmployers Association",
+    url="https://github.com/DirectEmployers/django-middleware",
+    license="Copyright © 2011-2024, DirectEmployers Association",
     packages=find_packages(exclude=["tests*"]),
-    python_requires=">=3.8",
-    install_requires=["django==3.2.*"],
+    python_requires=">=3.11",
+    install_requires=["django==4.2.*"],
 )
