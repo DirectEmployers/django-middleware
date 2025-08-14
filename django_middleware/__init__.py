bc70dd1676ebe2d87dcff5cbd85f65018138d315
Tim Loyer <tloyer@directemployers.org>
2025-08-14 14:47:13 -0400

    update text references to old package name (django-utils) and old Django version

--- django_middleware/__init__.py
+++ django_middleware/__init__.py
@@ -1,11 +1,11 @@
 """
 Module for code that can be shared between Django services.
 
-`django-utils` is a package intended to be imported and reused throughout
+`django-middleware` is a package intended to be imported and reused throughout
 multiple microservices. To maintain decoupled deployments, this means that every
-module in `django-utils` must comply with the following constraints:
+module in `django-middleware` must comply with the following constraints:
 
-1. May not import code from any other DE code, outside of the `django-utils` directory.
+1. May not import code from any other DE code, outside the `django-middleware` directory.
 2. May not directly interact with any databases or Django models. If data is
     required, the module should request it from the appropriate microservice API.
 3. Modules must avoid depending on the de.works monolith in any way!
